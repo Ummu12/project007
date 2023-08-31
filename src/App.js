5891320c10e044b9012a17acddb1327e76b137fd
Syed Umar <khan.afrin02@gmail.com>
2023-08-31 13:29:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Button, Alert, ProgressBar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/style.css";
+
+function App() {
+  const [now, setNow] = useState(0);
+
+  setTimeout(() => {
+    now < 100 && setNow(now + 2);
+  }, 1000);
+  return (
+    <div className="main">
+      <Button variant="warning">Add Animal</Button>
+      <Alert variant="success">Successfully added the user!</Alert>
+      <Alert variant="danger">Network is down. Please contact Admin.</Alert>
+      <ProgressBar variant={now < 50 ? "danger" : "success"} now={now} />
+      <br />
+      <Alert variant="warning">Complete your test before 1 pm</Alert>
+      <ProgressBar variant="success" now={40} />
+      <ProgressBar variant="info" now={70} />
+    </div>
+  );
+}
+
+export default App;
